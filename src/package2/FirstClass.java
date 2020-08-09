4ba753ab601c5cc49fd939e6498981efab240b38
ajamshid <69397472+ajamshid@users.noreply.github.com>
2020-08-09 11:54:09 -0400

    Update FirstClass.java
    second commit from remote master

--- src/package2/FirstClass.java
+++ src/package2/FirstClass.java
@@ -7,5 +7,11 @@ public class FirstClass {
         System.out.println("this is first changes from local master");
         System.out.println("This is the second commit from local master");
         System.out.println("This is the third commit from local master");
+        System.out.println("This is the first commit from remote");
+        
+        
+          System.out.println("This is the second commit from remote");
+        
+        
     }
 }
